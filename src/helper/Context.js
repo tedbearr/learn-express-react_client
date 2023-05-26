f881085dd48b8c10fa2d04f112ada877b680d902
tedbearr <teddystwn48@gmail.com>
2023-05-26 13:45:02 +0700

    update

--- src/helper/Context.js
+++ src/helper/Context.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-const base_url = createContext("https://103.54.170.25:3009/");
+const base_url = createContext("http://103.54.170.25:3009/");
 const authContext = createContext();
 let isExpanded = createContext(false);
 
